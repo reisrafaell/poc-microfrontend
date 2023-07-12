75050034a71f2dfc5f1a6fd707d1f955c9b10ae1
reisrafaell <rafael.r@compasso.com.br>
2023-07-12 18:02:17 -0300

    feat: adding angular header

---
+++ new-header/src/app/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  public headerText: any;
+  private unsubscribe$ = new Subject<void>();
+
+  constructor() {
+    this.getHeaderText()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((data: any) => {
+        this.headerText = data;
+      });
+  }
+
+  private getHeaderText(): Subject<any> {
+    const subject = new Subject<any>();
+    window.addEventListener('homeData', (event: any) => {
+      subject.next(event.detail.describe);
+    });
+    return subject;
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+}
---
+++ new-header/src/main.single-spa.ts
@@ -0,0 +1,20 @@
+import { enableProdMode, NgZone } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { singleSpaAngular } from 'single-spa-angular';
+
+import { AppModule } from './app/app.module';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: (singleSpaProps) => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return platformBrowserDynamic().bootstrapModule(AppModule);
+  },
+  template: '<app-root />',
+  NgZone,
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
