6e9a24f707adaee2f076de40ade0b2615d34b103
reisrafaell <rafael.r@compasso.com.br>
2023-07-13 11:39:06 -0300

    feat: adding install scripts and angular header

--- footer/src/components/templates/Footer/index.tsx
+++ footer/src/components/templates/Footer/index.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./styles";
 import { FooterProps } from "./types";
 
 const Footer = ({ children }: FooterProps) => {
+  const [data, setData] = useState("");
+
+  window.addEventListener("homeData", (event: any) => {
+    setData(event.detail.describe);
+    console.log(event);
+  });
+
   return (
     <S.FooterContainer>
       {children}
-      <h1>Footer</h1>
+      <h1>Footer: {data} </h1>
     </S.FooterContainer>
   );
 };
