d1deab69050cc12e6e87109c37f9b08d6f40b8ac
reisrafaell <rafael.r@compasso.com.br>
2023-07-13 11:38:38 -0300

    feat: adding install scripts and angular header

--- main-page/src/components/templates/Home/index.tsx
+++ main-page/src/components/templates/Home/index.tsx
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Parcel from "single-spa-react/parcel";
 
 import * as S from "./styles";
 import { HomeProps } from "./types";
 
 const Home = ({ children }: HomeProps) => {
+  const [data, setData] = useState("");
+
   const parcelConfig = async () => {
-    const module = await System.import("@poc/footer")
+    const module = await System.import("@poc/footer");
     return {
       bootstrap: module.bootstrap,
       mount: module.mount,
       unmount: module.unmount,
     };
   };
+  const parcelConfigHeader = async () => {
+    const module = await System.import("@new-header");
+    return {
+      bootstrap: module.bootstrap,
+      mount: module.mount,
+      unmount: module.unmount,
+    };
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatchEvent(
+      new CustomEvent("homeData", {
+        detail: {
+          id: 1,
+          describe: data,
+          test: "teste",
+        },
+      })
+    );
+  };
   return (
     <S.Container>
+      <Parcel config={parcelConfigHeader} />
       {children}
       <h1>Home</h1>
+      <form  onSubmit={handleSubmit}>
+        <label htmlFor="input-text">Description:</label>
+        <input id="input-text" onChange={(e: any) => setData(e.target.value)} />
+        <button>Ok</button>
+      </form>
       <Parcel config={parcelConfig} />
     </S.Container>
   );
--- main-page/src/components/templates/Home/styles.tsx
+++ main-page/src/components/templates/Home/styles.tsx
@@ -3,7 +3,24 @@ import styled from "styled-components";
 export const Container = styled.section`
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: space-between;
   min-height: 100vh;
   background-color: #d5d5d3;
+
+  form {
+    border: 2px solid #000;
+    width: 30rem;
+    height: 20rem;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    gap: 2rem;
+    padding: 2rem;
+    box-sizing: border-box;
+  }
+  input,
+  button {
+    height: 2rem;
+  }
 `;
